9d8c9501ef33b5adb65e0375810bc4d17d0a3781
KhanPython <81525782+KhanPython@users.noreply.github.com>
2022-06-19 08:51:44 +0500

    Switch to CollectionService

--- src/StarterPlayer/StarterCharacterScripts/SwimHandler/Settings.lua
+++ src/StarterPlayer/StarterCharacterScripts/SwimHandler/Settings.lua
@@ -1,6 +1,6 @@
 local settingsMod = {
 	
-	WATER_FOLDER = workspace:FindFirstChild("BOW"); -- Directory to your folder with water baseParts
+	SWIM_TAG = "SwimBasePart"; -- Name of the CollectionService tag for the swimmable baseparts
 	WATER_DRAG_FORCE_MULTIPLIER = 1; -- Additional force multiplier to drag/push the player when in water
 	WATER_JUMP_FORCE = .5; -- The force factor applied to the player when they attempt to jump in water 
 	PLAYER_FLOAT_FACTOR = 0 -- Anything over n>0 will make the player float up, and vice versa with n<0
